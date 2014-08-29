694568106225600cb592fcca408178243a8ae849
davidwhogg <david.hogg@nyu.edu>
2014-08-29 07:45:29 -0400

    insert long-period planet distortion

--- py/hotcold.py
+++ py/hotcold.py
@@ -21,22 +21,27 @@ def trapezoid(times, period, offset, depth, duration, gress):
 def get_fractions(times, period, offset, depth, duration, gress):
     return trapezoid(times, period, offset, depth, duration, gress)
 
-def integrate_fractions(times, exptime, period, offset, depth, duration, gress, K):
+def distort_times(times, period, Aamp, Bamp):
+    thetas = 2 * np.pi * times / period
+    return times + Aamp * np.cos(thetas) + Bamp * np.cos(thetas)
+
+def integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp, K):
     delta_times = np.arange(-0.5 * exptime + 0.5 * exptime / K, 0.5 * exptime, exptime / K)
     bigtimes = times[:, None] + delta_times[None, :]
-    bigfracs = get_fractions(bigtimes, period, offset, depth, duration, gress)
+    bigtimes = distort_times(bigtimes, coldperiod, Aamp, Bamp)
+    bigfracs = get_fractions(bigtimes, hotperiod, offset, depth, duration, gress)
     return np.mean(bigfracs, axis=1)
 
-def observe_star(times, exptime, sigma, period, offset, depth, duration, gress, K=21): # MAGIC
+def observe_star(times, exptime, sigma, hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp, K=21): # MAGIC
     fluxes = np.ones_like(times)
-    fluxes *= (1. - integrate_fractions(times, exptime, period, offset, depth, duration, gress, K))
+    fluxes *= (1. - integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp, K))
     fluxes += sigma * np.random.normal(size=fluxes.shape)
     return fluxes
 
 def ln_like(data, pars):
     times, fluxes, ivars = data
-    period, offset, depth, duration, gress = pars
-    fracs = integrate_fractions(times, exptime, period, offset, depth, duration, gress, 5) # MAGIC
+    hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp = pars
+    fracs = integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp, 5) # MAGIC
     return -0.5 * np.sum(ivars * (fluxes - (1. - fracs)) ** 2)
 
 def ln_prior(pars):
@@ -53,7 +58,11 @@ if __name__ == "__main__":
     times = np.arange(0., 4.1 * 365, 1.0 / 48.) # 30-min cadence in d
     exptime = ((1.0 / 24.) / 60.) * 27. # 27 min in d
     sigma = 1.e-5
-    truepars = np.array([6.5534, 731.55, 0.005235, 0.32322, 0.05232]) # MAGIC
+    hotperiod = 6.5534 # MAGIC
+    coldperiod = 365.25 * 5.0 # MAGIC
+    Aamp = 2.34 / 86400. # MAGIC
+    Bamp = 0.
+    truepars = np.array([hotperiod, 731.55, 0.005235, 0.32322, 0.05232, coldperiod, Aamp, Bamp]) # MAGIC
     fluxes = observe_star(times, exptime, sigma, *truepars)
     plt.plot(times, fluxes, ".")
     plt.savefig("hotcold_data.png")
@@ -73,10 +82,12 @@ if __name__ == "__main__":
         nwalkers *= 2
         pos = chain[np.random.randint(low, high=len(chain), size=nwalkers)]
         resids = (sampler.flatchain - truepars[None, :])
-        resids[:, [0, 1, 3, 4]] *= 86400.
+        resids[:, [0, 1, 3, 4, 6, 7]] *= 86400.
         resids[:, 2] *= 1.e6
         fig = triangle.corner(resids,
-                              labels=["period resid (s)", "offset resid (s)", 
-                                      "depth resid (ppm)", "duration resid (s)", "gress resid (s)"],
+                              labels=["hot period resid (s)", "offset resid (s)", 
+                                      "depth resid (ppm)", "duration resid (s)", 
+                                      "gress resid (s)", "cold period resid (d)",
+                                      "A amplitude resid (s)", "B amplitude resid (s)"],
                               truths=(truepars - truepars))
         fig.savefig("hotcold_triangle.png")
