b92b56089add0e5844f926a86366e3ec4ee5792f
davidwhogg <david.hogg@nyu.edu>
2014-08-30 06:25:50 -0400

    move to ln cold period; more jupiter-like

--- py/hotcold.py
+++ py/hotcold.py
@@ -1,6 +1,12 @@
 """
 This file is part of the StellarClocks project.
 Copyright 2014 David W. Hogg (NYU).
+
+# to-do items
+- turn on multiprocessing for emcee
+- fork the plotting
+- figure out real-data pre-processing or update likelihood function noise model
+- run on real data
 """
 
 import numpy as np
@@ -21,30 +27,33 @@ def trapezoid(times, period, offset, depth, duration, gress):
 def get_fractions(times, period, offset, depth, duration, gress):
     return trapezoid(times, period, offset, depth, duration, gress)
 
-def distort_times(times, period, Aamp, Bamp):
-    thetas = 2 * np.pi * times / period
+def distort_times(times, ln_period, Aamp, Bamp):
+    thetas = 2 * np.pi * times / np.exp(ln_period)
     return times - Aamp * np.cos(thetas) - Bamp * np.sin(thetas)
 
-def integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp, K):
+def integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, ln_coldperiod, Aamp, Bamp, K):
     delta_times = np.arange(-0.5 * exptime + 0.5 * exptime / K, 0.5 * exptime, exptime / K)
     bigtimes = times[:, None] + delta_times[None, :]
-    bigtimes = distort_times(bigtimes, coldperiod, Aamp, Bamp)
+    bigtimes = distort_times(bigtimes, ln_coldperiod, Aamp, Bamp)
     bigfracs = get_fractions(bigtimes, hotperiod, offset, depth, duration, gress)
     return np.mean(bigfracs, axis=1)
 
-def observe_star(times, exptime, sigma, hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp, K=21): # MAGIC
+def observe_star(times, exptime, sigma, hotperiod, offset, depth, duration, gress, ln_coldperiod, Aamp, Bamp, K=21): # MAGIC
     fluxes = np.ones_like(times)
-    fluxes *= (1. - integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp, K))
+    fluxes *= (1. - integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, ln_coldperiod, Aamp, Bamp, K))
     fluxes += sigma * np.random.normal(size=fluxes.shape)
     return fluxes
 
 def ln_like(data, pars):
     times, fluxes, ivars = data
-    hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp = pars
-    fracs = integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, coldperiod, Aamp, Bamp, 5) # MAGIC
+    hotperiod, offset, depth, duration, gress, ln_coldperiod, Aamp, Bamp = pars
+    fracs = integrate_fractions(times, exptime, hotperiod, offset, depth, duration, gress, ln_coldperiod, Aamp, Bamp, 5) # MAGIC
     return -0.5 * np.sum(ivars * (fluxes - (1. - fracs)) ** 2)
 
 def ln_prior(pars):
+    hotperiod, offset, depth, duration, gress, ln_coldperiod, Aamp, Bamp = pars
+    if ln_coldperiod < 5. or ln_coldperiod > 10.:
+        return -np.Inf
     return 0.
 
 def ln_posterior(pars, data):
@@ -56,13 +65,16 @@ def ln_posterior(pars, data):
 if __name__ == "__main__":
     np.random.seed(42)
     times = np.arange(0., 4.1 * 365, 1.0 / 48.) # 30-min cadence in d
+    times -= np.median(times) # put zero in the center of the plot
     exptime = ((1.0 / 24.) / 60.) * 27. # 27 min in d
     sigma = 1.e-5
     hotperiod = 6.5534 # MAGIC
-    coldperiod = 365.25 * 5.0 # MAGIC
-    Aamp = 12.34 / 86400. # MAGIC
-    Bamp = 0.
-    truepars = np.array([hotperiod, 731.55, 0.005235, 0.32322, 0.05232, coldperiod, Aamp, Bamp]) # MAGIC
+    ln_coldperiod = np.log(365.25 * 11.8618) # MAGIC (Jupiter)
+    coldamp = 5.0 / 86400. # MAGIC (twice Jupiter's amplitude)
+    coldphase = 0.1 # radian MAGIC
+    Aamp = coldamp * np.cos(coldphase)
+    Bamp = coldamp * np.sin(coldphase)
+    truepars = np.array([hotperiod, 731.55, 0.005235, 0.32322, 0.05232, ln_coldperiod, Aamp, Bamp]) # MAGIC
     true_time_delays = times - distort_times(times, *(truepars[5:]))
     fluxes = observe_star(times, exptime, sigma, *truepars)
     fig1 = plt.figure(1)
@@ -75,18 +87,20 @@ if __name__ == "__main__":
     data = np.array([times, fluxes, ivars])
     initpars = 1. * truepars
     initpars[6:] = [0., 0.] # zero out amplitudes
-    ndim, nwalkers = len(initpars), 16
-    pos = [initpars + 1e-5*np.random.randn(ndim) for i in range(nwalkers)]
-    nburn = 10
+    ndim, nwalkers, nlinks = len(initpars), 16, 512
+    pos = initpars[None, :] + 1e-6 * np.random.normal(size=(nwalkers, ndim))
+    pos[:, 5] += -0.5 + np.random.uniform(size=nwalkers) # more scatter for ln_coldperiod
+    nburn = 100
     for burn in range(nburn):
-        nlinks = 64
         print "burning %d, ndim %d, nwalkers %d, nlinks %d" % (burn, ndim, nwalkers, nlinks)
         sampler = emcee.EnsembleSampler(nwalkers, ndim, ln_posterior, args=(data,))
         sampler.run_mcmc(pos, nlinks)
         chain = sampler.flatchain
         low = 3 * len(chain) / 4
-        if nwalkers < 512:
+        if nwalkers < 128:
             nwalkers *= 2
+        if nlinks > 64:
+            nlinks /= 2
         pos = chain[np.random.randint(low, high=len(chain), size=nwalkers)]
 
         # plot samples
@@ -107,7 +121,7 @@ if __name__ == "__main__":
         fig = triangle.corner(resids,
                               labels=["hot period resid (s)", "offset resid (s)", 
                                       "depth resid (ppm)", "duration resid (s)", 
-                                      "gress resid (s)", "cold period resid (d)",
+                                      "gress resid (s)", "ln cold period resid",
                                       "A amplitude resid (s)", "B amplitude resid (s)"],
                               truths=(truepars - truepars))
         fig.savefig("hotcold_triangle.png")
